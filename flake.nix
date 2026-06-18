40bcd150d05dc9b0c81cbfe07fa995d91bb84e4a
zion <ox7a696f6e@outlook.com>
2026-06-18 19:09:09 +0800

    chore: simplify flake.nix with flake-utils, relax cabal bounds

--- flake.nix
+++ flake.nix
@@ -1,65 +1,32 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
-    self,
     nixpkgs,
-  }: let
-    supportedSystems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
-
-    forEachSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f nixpkgs.legacyPackages.${system});
-
-    ghcVersion = "ghc96";
-
-    forHaskellPkgs = f: pkgs: f pkgs pkgs.haskell.packages.${ghcVersion};
-  in {
-    lib = import ./lib.nix {inherit (self) packages;};
-
-    libWith = pkgs:
-      import ./lib.nix {
-        inherit pkgs;
-        inherit (self) packages;
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      haskellPkgs = pkgs.haskellPackages;
+      nix-ast = haskellPkgs.callCabal2nix "nix-ast" ./. {};
+    in {
+      packages = {
+        default = nix-ast;
+        inherit nix-ast;
       };
-
-    packages = forEachSystem (
-      forHaskellPkgs (
-        pkgs: haskellPkgs: let
-          nix-ast = haskellPkgs.callCabal2nix "nix-ast" ./. {};
-        in {
-          default = nix-ast;
-          inherit nix-ast;
-        }
-      )
-    );
-
-    devShells = forEachSystem (
-      forHaskellPkgs (
-        pkgs: haskellPkgs: {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              haskell.compiler.${ghcVersion}
-              haskellPkgs.cabal-fmt
-              cabal-install
-              fourmolu
-              haskellPkgs.haskell-language-server
-              hlint
-              pkg-config
-            ];
-
-            buildInputs = with pkgs; [
-              libsodium
-              zlib
-            ];
-          };
-        }
-      )
-    );
-  };
+      devShells.default = haskellPkgs.shellFor {
+        packages = p: [nix-ast];
+        buildInputs = with pkgs; [
+          cabal-install
+          fourmolu
+          ghcid
+          haskellPkgs.hlint
+          haskellPkgs.cabal-fmt
+        ];
+      };
+    });
 }

b3218cbe7433b50555f87fd6e0cb71745cc7007e
zion <ox7a696f6e@outlook.com>
2026-06-29 02:49:28 +0800

    feat: use fileset to narrow build src

--- flake.nix
+++ flake.nix
@@ -4,38 +4,56 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-      ...
-    }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    let
+      src = nixpkgs.lib.fileset.toSource {
+        root = ./.;
+        fileset = nixpkgs.lib.fileset.unions [
+          ./CHANGELOG.md
+          ./LICENSE
+          ./app
+          ./nix-ast.cabal
+          ./src
+          ./test
+        ];
+      };
+    in
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        haskellPkgs = pkgs.haskellPackages;
-        nix-ast = haskellPkgs.callCabal2nix "nix-ast" ./. { };
+        hpkgs = pkgs.haskellPackages;
+        nix-ast = hpkgs.callCabal2nix "nix-ast" src { };
       in
       {
         packages = {
           default = nix-ast;
           inherit nix-ast;
         };
-        devShells.default = haskellPkgs.shellFor {
+
+        checks.tests = pkgs.runCommand "nix-ast-tests" {
+          requiredTestResults = import ./nix/tests.nix {
+            inherit pkgs;
+            inherit (self) lib;
+          };
+        } "echo all tests passed > $out";
+
+        devShells.default = hpkgs.shellFor {
           packages = p: [ nix-ast ];
           buildInputs = with pkgs; [
             cabal-install
             fourmolu
             ghcid
-            haskellPkgs.hlint
-            haskellPkgs.cabal-fmt
+            hlint
+            hpkgs.cabal-fmt
           ];
         };
       }
     )
     // {
-      lib = import ./nix/lib { inherit (self) packages; };
+      lib = import ./nix/lib {
+        inherit (nixpkgs) lib;
+        inherit (self) packages;
+      };
     };
 }
